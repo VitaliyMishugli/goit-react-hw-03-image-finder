d95e5a6a73e97ea49389e4a4f97d75109329e3f8
Vitaliy Mishugli <vmishughli@gmail.com>
2023-01-28 21:14:36 +0000

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,26 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      Module 3. 2-Image-finder
-    </div>
-  );
+import { Component } from "react";
+
+export default class App extends Component {
+
+  state = {
+    result: null,
+  }
+
+  async componentDidMount() {
+    await fetch('https://pixabay.com/api/?q=cat&page=1&key=29908422-6515e5e6655e3a8d0d58918bc&image_type=photo&orientation=horizontal&per_page=12')
+      .then(res => res.json())
+      .then(res => this.setState({ result: res.hits[0] }));
+    console.log(this.state.result, Date.now());
+  }
+
+  render() {
+    return (
+      <>
+        {this.state.result ? <h2>{this.state.result.id}</h2> : <h2>Очікуємо завантаження...</h2> }
+        {/* < img src={`${this.result.webformatURL}`} />
+        <h2>{ this.result.id}</h2> */}
+      </>
+    );
+  }
+
 };
